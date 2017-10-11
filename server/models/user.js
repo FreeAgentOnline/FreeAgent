e9126910cd836f774399c51a97b581727591bfec
Sean McPherson <seanmcp25@gmail.com>
2017-10-11 11:45:38 -0400

    Set up User model schema

---
+++ server/models/user.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+//================================
+// User Schema
+//================================
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  username: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    firstName: { type: String },
+    lastName: { type: String }
+  },
+  bio: String,
+  events: [String],
+  records: [{
+    date: Date,
+    eventName: String,
+    performance: String,
+    location: String,
+    reference: String,
+  }],
+  resetPasswordToken: { type: String },
+  resetPasswordExpires: { type: Date }
+},
+{
+  timestamps: true
+});
+
+// Pre-save of user to database, hash password if password is modified or new
+UserSchema.pre('save', function(next) {
+  const user = this;
+  const SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Method to compare password for login
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return cb(err); }
+
+    cb(null, isMatch);
+  });
+}
+
+module.exports = mongoose.model('User', UserSchema);
