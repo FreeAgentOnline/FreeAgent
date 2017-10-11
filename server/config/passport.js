dd74cf10b3e7ae95e33b4ae08d9273b0699d5fde
Sean McPherson <seanmcp25@gmail.com>
2017-10-11 11:51:12 -0400

    Set up passport configuration

---
+++ server/config/passport.js
@@ -0,0 +1,47 @@
+// Importing Passport, strategies, and config
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('./main');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+const localOptions = { usernameField: 'email' };
+
+// Setting up local login strategy
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if(err) { return done(err); }
+    if(!user) { return done(null, false, { error: 'Your login details could not be verified. Please try again.' }); }
+
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false, { error: "Your login details could not be verified. Please try again." }); }
+
+      return done(null, user);
+    });
+  });
+});
+
+const jwtOptions = {
+  // Telling Passport to check authorization headers for JWT
+  jwtFromRequest: ExtractJwt.fromAuthHeader(),
+  // Telling Passport where to find the secret
+  secretOrKey: config.secret
+};
+
+// Setting up JWT login strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload._id, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+passport.use(jwtLogin);
+passport.use(localLogin);
--- server/models/user.js
+++ server/models/user.js
@@ -47,13 +47,13 @@ UserSchema.pre('save', function(next) {
   const user = this;
   const SALT_FACTOR = 5;
 
-  if (!user.isModified('password')) return next();
+  if (!user.isModified('password')) { return next(); }
 
   bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
-    if (err) return next(err);
+    if (err) { return next(err); }
 
     bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) return next(err);
+      if (err) { return next(err); }
       user.password = hash;
       next();
     });
