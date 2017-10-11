616278c420f86d546e0af67bbd0f8bacaa245b32
Sean McPherson <seanmcp25@gmail.com>
2017-10-11 11:33:04 -0400

    Set up server index.js

---
+++ server/config/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Secret key for JWT signing and encryption
+  'secret': 'pass the baton',
+  // Database connection information
+  'database': 'mongodb://localhost:27017/free-agent',
+  // Setting port for server
+  'port': process.env.PORT || 3000
+}
---
+++ server/index.js
@@ -0,0 +1,28 @@
+// Importing Node modules and initializing Express
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./config/main');
+
+// Database Connection
+mongoose.connect(config.database, { useMongoClient: true });
+
+// Start the server
+const server = app.listen(config.port);
+console.log('Your server is running on port ' + config.port + '.');
+
+// Setting up basic middleware for all Express requests
+app.use(logger('dev')); // Log requests to API using morgan
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Enable CORS from client-side
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
