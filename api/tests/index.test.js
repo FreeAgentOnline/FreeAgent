44211f3e416943979c6e6b5196d6a3e2d25cd195
Sean McPherson <seanmcp25@gmail.com>
2017-10-16 08:47:01 -0400

    Create an pass tests on user endpoints

--- api/tests/index.test.js
+++ api/tests/index.test.js
@@ -1,52 +1,127 @@
 const request = require('supertest')
 const app = require('../server')
 
-describe('GET /', () => {
-  test('Should send object sucessfully', () => {
+// .set({"Authorization": "Basic YW5uZTphbm5l"})
+
+describe('GET /user', () => {
+  test('Should return an array of all users', () => {
     return request(app)
-      .get('/api')
+      .get('/api/user')
       .expect(200)
-      // .set({"Authorization": "Basic YW5uZTphbm5l"})
       .then(res => {
-        expect(res.body).toHaveProperty('status')
-        expect(res.body.status).toBe('success')
-        expect(res.body).toHaveProperty('data')
-        expect(res.body.data).toBeTruthy()
+        expect(res.body).toBeTruthy()
       })
   })
 })
 
-describe('GET /period/:id', () => {
-  test('Should find information about one period with given id', () => {
-    return request(app)
-      .get('/api/period/59c6bdb6e4af98547c7d15dd')
-      .expect(200)
-      .then(res => {
-        expect(res.body).toHaveProperty('status')
-        expect(res.body.status).toBe('success')
-        expect(res.body).toHaveProperty('data')
-        expect(res.body.data._id).toBeTruthy()
-      })
-  })
-})
+let createdUserId;
 
-describe('POST /period/:id/student', () => {
-  test('Should create a new student in the given period', () => {
+describe('POST /user', () => {
+  test('Should create a new user', () => {
     return request(app)
-      .post('/api/period/59c6bdb6e4af98547c7d15dd/student')
+      .post('/api/user')
       .type('form')
       .send({
-        name: 'Test Student',
-        female: ''
+        first_name: 'Test',
+        last_name: 'McTesterson',
+        username: 'testuser',
+        email: 'unique@test.email',
+        password: 'securepassword'
       })
+      .expect(200)
       .then(res => {
-        expect(res.body).toHaveProperty('status')
-        expect(res.body.status).toBe('success')
-        expect(res.body).toHaveProperty('data')
-        expect(res.body.data.name).toBeTruthy()
+        createdUserId = res.body._id;
+        expect(res.body).toHaveProperty('_id')
+        expect(res.body._id).toBeTruthy()
+        expect(res.body).toHaveProperty('first_name')
+        expect(res.body.first_name).toBe('Test')
+        expect(res.body).toHaveProperty('last_name')
+        expect(res.body.last_name).toBe('McTesterson')
+        expect(res.body).toHaveProperty('username')
+        expect(res.body.username).toBe('testuser')
+        expect(res.body).toHaveProperty('email')
+        expect(res.body.email).toBe('unique@test.email')
+        expect(res.body).toHaveProperty('password')
+        expect(res.body.password).toBeTruthy()
+        expect(res.body).toHaveProperty('bio')
+        expect(res.body.bio).toBe('')
       })
   })
 })
+
+describe('PATCH /user/:_id', () => {
+  test('Should update user details', () => {
+    return request(app)
+      .patch('/api/user/' + createdUserId)
+      .type('form')
+      .send({
+        first_name: 'T.',
+        last_name: 'McT.',
+        bio: 'Updated biography'
+      })
+      .expect(200)
+      .then(res => {
+        expect(res.body).toHaveProperty('_id')
+        expect(res.body._id).toBeTruthy()
+        expect(res.body).toHaveProperty('first_name')
+        expect(res.body.first_name).toBe('T.')
+        expect(res.body).toHaveProperty('last_name')
+        expect(res.body.last_name).toBe('McT.')
+        expect(res.body).toHaveProperty('username')
+        expect(res.body.username).toBe('testuser')
+        expect(res.body).toHaveProperty('email')
+        expect(res.body.email).toBe('unique@test.email')
+        expect(res.body).toHaveProperty('password')
+        expect(res.body.password).toBeTruthy()
+        expect(res.body).toHaveProperty('bio')
+        expect(res.body.bio).toBe('Updated biography')
+      })
+  })
+})
+
+describe('DELETE /user/:_id', () => {
+  test('Should delete specific user', () => {
+    return request(app)
+      .delete('/api/user/' + createdUserId)
+      .expect(200)
+      .then(res => {
+        expect(res.body).toHaveProperty('n')
+        expect(res.body).toHaveProperty('ok')
+      })
+  })
+})
+
+// describe('GET /period/:id', () => {
+//   test('Should find information about one period with given id', () => {
+//     return request(app)
+//       .get('/api/period/59c6bdb6e4af98547c7d15dd')
+//       .expect(200)
+//       .then(res => {
+//         expect(res.body).toHaveProperty('status')
+//         expect(res.body.status).toBe('success')
+//         expect(res.body).toHaveProperty('data')
+//         expect(res.body.data._id).toBeTruthy()
+//       })
+//   })
+// })
+//
+// describe('POST /period/:id/student', () => {
+//   test('Should create a new student in the given period', () => {
+//     return request(app)
+//       .post('/api/period/59c6bdb6e4af98547c7d15dd/student')
+//       .type('form')
+//       .send({
+//         name: 'Test Student',
+//         female: ''
+//       })
+//       .then(res => {
+//         expect(res.body).toHaveProperty('status')
+//         expect(res.body.status).toBe('success')
+//         expect(res.body).toHaveProperty('data')
+//         expect(res.body.data.name).toBeTruthy()
+//       })
+//   })
+// })
 // Need to have a student id to delete
 // describe('"DELETE"(PUT) /period/:periodId/student/:studentId', () => {
 //   test('Should remove student with given id from period student array', () => {
