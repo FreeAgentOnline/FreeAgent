09115cc30c087ee3fa29fcc638cb0d1c0363e67b
Imani Oliver <imani.oliver@gmail.com>
2017-10-18 10:35:49 -0400

    edits handleSearch on search component

--- server/client/src/components/Search.js
+++ server/client/src/components/Search.js
@@ -15,7 +15,7 @@ export default class Search extends Component {
 
     handleSearch = e => {
       // search for events
-      this.setState({ query: e.target.value });
+      this.setState({ query: '' });
     }
     render(){
 
