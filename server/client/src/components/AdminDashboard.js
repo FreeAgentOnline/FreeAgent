559c34e32a84a6c07d4e585a41771042de409edd
Imani Oliver <imani.oliver@gmail.com>
2017-10-18 10:20:03 -0400

    confirmation of file structure

---
+++ server/client/src/components/AdminDashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { meets } from '../../data/meets';
+
+export default class AdminDashboard extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+             user
+        }
+    }
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
---
+++ server/client/src/components/Search.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            query: this.props.query
+        }
+
+
+
+
+
+    }
+
+    handleQuery = e => {
+        this.setState({ query : ''})
+    }
+
+    handleSearch = () => {
+      // search for events
+      this.setState({ query: '' });
+    }
+    render(){
+
+
+
+
+        return (
+            <div id="searchPage">
+                <div id="left" className="panel panel-default">
+                    <div className="panel-body"> Search Page Left.
+                         <input type="text" className="form-control" id="searchQuery" placeholder="Search for meets" value={this.state.query} onChange={this.handleQuery}/>
+                        <button type="submit" className="btn btn-primary" id="searchButton" onClick={this.handleSearch}>Search</button>
+                        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHxd9VPfDLGh5BJwhRWIJwdnL7r0my160&callback=initMap"
+  type="text/javascript"></script>
+                    </div>
+
+                </div>
+                <div id="middleBar">
+                </div>
+
+                <div id="right" className="panel panel-default">
+                    <div className="panel-body">
+                    <h1>Available Meets</h1>
+                    <div classname = "Results">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="card">
+                                  <div class="card-block">
+                                    <h3 class="card-title">Special title treatment</h3>
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                  </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                  <div class="card-block">
+                                    <h3 class="card-title">Special title treatment</h3>
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                  </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                  <div class="card-block">
+                                    <h3 class="card-title">Special title treatment</h3>
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                  </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                  <div class="card-block">
+                                    <h3 class="card-title">Special title treatment</h3>
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                  </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
