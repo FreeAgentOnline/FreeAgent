d3a1d85d2943ed04b1d899acde720f1f683cb882
Sean McPherson <seanmcp25@gmail.com>
2017-10-19 16:05:37 -0400

    Add store to Home and search components

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { saveQuery } from '../actions';
 
 class Home extends Component {
   constructor(props) {
@@ -10,23 +13,49 @@ class Home extends Component {
   }
   handleQuery = e => {
     this.setState({ query: e.target.value });
+
+    // setTimeout required to preserve query on redirect
+    setTimeout(() => {
+      this.props.saveQuery(this.state.query);
+      this.props.history.push('/search');
+    }, 0);
+
   }
   handleSearch = () => {
-    // search for events
+    // Set store.query based on input
+    this.props.saveQuery(this.state.query);
     this.setState({ query: '' });
+    // search for events
+
+    // this.props.history.push() redirects to a new endpoint in React w/ Redux
+    this.props.history.push('/search');
   }
   render() {
+    console.log('props on Home', this.props);
     return (
       <div className="container">
         <h2 className="display-2">Welcome to FreeAgent</h2>
         <p className="lead">FreeAgent is for professional athletes looking to make their mark in the world of track & field. FreeAgent helps you find meets in your area, provides registration information, and allows you to track and share your personal records with potential sponsors.</p>
         <div className="d-flex">
           <input type="text" className="form-control" id="searchQuery" placeholder="Search for meets" value={this.state.query} onChange={this.handleQuery}/>
-          <Link to="/search" className="btn btn-primary" id="searchButton" query= {this.state.query}>Search</Link>
+          <button className="btn btn-primary" id="searchButton" onClick={this.handleSearch}>Search</button>
         </div>
       </div>
     );
   }
 }
 
-export default Home;
+function mapStateToProps(state) {
+  console.log('state on Home', state);
+  return {
+    query: state.query
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    saveQuery
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
