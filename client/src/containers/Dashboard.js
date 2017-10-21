6aacaedfd204b06c99b4b5285b215ec370dd5fac
Sean McPherson <seanmcp25@gmail.com>
2017-10-21 18:36:22 -0400

    Add images

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import user from '../data/user';
 
+
+import '../styles/dashboard.css';
+
 import ResultDashboard from '../components/ResultDashboard';
 
 class Dashboard extends Component {
@@ -21,6 +24,32 @@ class Dashboard extends Component {
           <Link to={`/${this.state.user.username}`} className="btn btn-secondary mr-2">Profile</Link>
           <Link to="/settings" className="btn btn-secondary">Settings</Link>
         </p>
+        <div id="dashBoxContainer" className="">
+          <div id="dashBasic" className="dashBox">
+            <div className="">
+              <h2 className="">Basic Information</h2>
+              <p className="">Update your name, tagline, bio, and location</p>
+            </div>
+          </div>
+          <div id="dashTeam" className="dashBox">
+            <div className="">
+              <h2 className="">Team history</h2>
+              <p className="">Update and add to your team history</p>
+            </div>
+          </div>
+          <div id="dashResults" className="dashBox">
+            <div className="">
+              <h2 className="">Log results</h2>
+              <p className="">Track your results and display them on your profile</p>
+            </div>
+          </div>
+          <div id="dashSettings" className="dashBox">
+            <div className="">
+              <h2 className="">Settings</h2>
+              <p className="">Update email address, reset password, and privacy settings</p>
+            </div>
+          </div>
+        </div>
         {/*<h4>Scheduled events</h4>*/}
         {/* Render Saved Meets */}
         {/*<h4>Starred events</h4>*/}
