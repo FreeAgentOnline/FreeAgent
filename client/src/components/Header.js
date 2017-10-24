f976c49e56a9e4ef6e6a6a0815d98ca0e8c28468
Sean McPherson <seanmcp25@gmail.com>
2017-10-24 17:08:03 -0400

    Design home page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,10 @@ class Header extends Component {
   render() {
     return (
       <div className="container-fluid d-flex justify-content-between align-items-center" id="site-header">
-        <NavLink to="/"><h4>FreeAgent</h4></NavLink>
+        <NavLink id="headerLink" to="/">
+          <img id="headerLogo" src={require("../images/freeagent_logo.png")} alt="FreeAgent"/>
+          <h4><span className="fw300">Free</span>Agent</h4>
+        </NavLink>
         <NavBar />
       </div>
     );
