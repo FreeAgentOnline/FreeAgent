15d9799c21254d87ab5b8b53c1f3c0df7df63820
Sean McPherson <seanmcp25@gmail.com>
2017-10-24 22:03:37 -0400

    Add sample saved meets to dashboard; style dashboard

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -21,29 +21,76 @@ class Dashboard extends Component {
           <Link to={`/${this.state.user.username}`} className="btn btn-secondary mr-2">Profile</Link>
           <Link to="/settings" className="btn btn-secondary">Settings</Link>
         </p>
-        <div id="dashBoxContainer" className="">
+        <h3>Saved meets</h3>
+        <div className="row font-weight-bold">
+          <div className="col">
+            Date
+          </div>
+          <div className="col">
+            Name
+          </div>
+          <div className="col">
+            Location
+          </div>
+          <div className="col">
+            Information
+          </div>
+          <div className="col">
+            Edit
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            10/26/2017
+          </div>
+          <div className="col">
+            The Iron Yard Demo Day
+          </div>
+          <div className="col">
+            The Gathering Spot - Atlanta, GA
+          </div>
+          <div className="col">
+            More information
+          </div>
+          <div className="col">
+            <button className="btn btn-outline-danger btn-sm">Remove</button>
+          </div>
+        </div>
+        <div id="dashBoxContainer" className="mt-3">
           <Link to="/dashboard/info" id="dashBasic" className="dashBox">
             <div className="">
-              <h2 className="">Basic Info</h2>
+              <div className="d-flex align-items-center">
+                <i className="fa fa-info-circle fa-lg fa-fw" aria-hidden="true"></i>
+                <h2 className="">Basic Info</h2>
+              </div>
               <p className="">Update your name, tagline, bio, and location</p>
             </div>
           </Link>
           <Link to="/dashboard/result" id="dashResults" className="dashBox">
           <div className="">
-          <h2 className="">Results</h2>
-          <p className="">Track your results and display them on your profile</p>
+            <div className="d-flex align-items-center">
+              <i className="fa fa-line-chart fa-lg fa-fw" aria-hidden="true"></i>
+              <h2 className="">Results</h2>
+            </div>
+            <p className="">Track your results and display them on your profile</p>
           </div>
           </Link>
           <Link to="/dashboard/team" id="dashTeam" className="dashBox">
             <div className="">
-              <h2 className="">Team history</h2>
+              <div className="d-flex align-items-center">
+                <i className="fa fa-history fa-lg fa-fw" aria-hidden="true"></i>
+                <h2 className="">History</h2>
+              </div>
               <p className="">Update and add to your team history</p>
             </div>
           </Link>
           <Link to="/dashboard/settings" id="dashSettings" className="dashBox">
             <div className="">
-              <h2 className="">Settings</h2>
-              <p className="">Update email, reset password, and privacy settings</p>
+              <div className="d-flex align-items-center">
+                <i className="fa fa-cog fa-lg fa-fw" aria-hidden="true"></i>
+                <h2 className="">Settings</h2>
+              </div>
+              <p className="">Update email, reset password, and other settings</p>
             </div>
           </Link>
         </div>
