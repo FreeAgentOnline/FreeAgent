fb192c745d3623524ec0b6d62c150ee3ef1f3a27
Sean McPherson <seanmcp25@gmail.com>
2017-10-25 12:17:27 -0400

    Add save meet function to ViewMeet component; add styles to ViewMeet component

--- client/src/components/ViewMeet.js
+++ client/src/components/ViewMeet.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import meets from '../data/meets';
+import user from '../data/user';
 import * as moment from 'moment';
 import { connect } from 'react-redux';
 
@@ -10,40 +10,43 @@ class ViewMeet extends Component {
         super(props)
 
         this.state = {
-            currentMeet: {}
+            currentMeet: {},
+            alreadySaved: false,
+            user
         }
     }
 
 
-    // handleSaveMeet = e => {
-    //     e.preventDefault();
-    //     let savedMeetBody = {
-    //         //I'll need a solution for the userId here since we aren't pulling from current users.
-    //         // userId: req.params.userId,
-    //         // //
-    //         // meetId: req.params.meetId,
-    //         isScheduled: false,
-    //         name: this.state.currentMeet.name,
-    //         date_start: this.state.currentMeet.date_start,
-    //         date_end: this.state.currentMeet.date_end,
-    //         country: this.state.currentMeet.country,
-    //         venue: this.state.currentMeet.venue || '',
-    //         city: this.state.currentMeet.city || '',
-    //         state: this.state.currentMeet.state || '',
-    //         lat: this.state.currentMeet.lat || '',
-    //         lng: this.state.currentMeet.lng || '',
-    //     }
-    //     fetch(`/api/meet/`+ this.state.currentMeet.meetId + `/save/user/` + this.props.user, {
-    //       method: 'POST',
-    //       body: JSON.stringify(savedMeetBody),
-    //       headers: {
-    //         'Content-Type': 'application/json'
-    //       }
-    //     })
-    //     .then(data => alert('Meet Saved'))
-    //     .catch(err => console.log("error posting saved meet to database: ", err))
-    //
-    // }
+    handleSaveMeet = e => {
+        e.preventDefault();
+        let savedMeetBody = {
+            username: this.state.user.username,
+            meetId: this.props.match.params.meetId,
+            isScheduled: false,
+            name: this.state.currentMeet.name,
+            date_start: this.state.currentMeet.date_start,
+            date_end: this.state.currentMeet.date_end,
+            country: this.state.currentMeet.country,
+            venue: this.state.currentMeet.venue || '',
+            city: this.state.currentMeet.city || '',
+            state: this.state.currentMeet.state || '',
+            lat: this.state.currentMeet.lat || '',
+            lng: this.state.currentMeet.lng || '',
+        }
+        fetch(`/api/meet/`+ this.state.currentMeet.meetId + `/save/user/` + this.state.user.username, {
+          method: 'POST',
+          body: JSON.stringify(savedMeetBody),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(data => {
+          this.setState({ alreadySaved: true });
+          alert('Meet Saved');
+        })
+        .catch(err => console.log("error posting saved meet to database: ", err))
+
+    }
     //
     // handleScheduleMeet = e => {
     //     e.preventDefault();
@@ -85,26 +88,19 @@ class ViewMeet extends Component {
         return(
           <div className="container">
             <div className="card m-3">
-                    <div className="card-block">
-                          <h3> Meet Information</h3>
-                          <h4 className="card-title"> <strong>Name: </strong> {meet.name}</h4>
+                  <div className="card-block">
+                        <h3>Meet Information</h3>
+                        <h2 className="card-title">{meet.name}</h2>
 
-                          <p className="card-text"><strong>Date Start: </strong>{moment(meet.date_start).format("ddd, MMMM D YYYY")}</p>
-                          <p className="card-text"><strong>Date End: </strong>{moment(meet.date_end).format("ddd, MMMM D YYYY")}</p>
-                          <p><strong>Cost: </strong>${meet.cost}</p>
-                          <p><strong>Venue: </strong>{meet.venue}</p>
+                        <p className="card-text"><strong>Date Start: </strong>{moment(meet.date_start).format("ddd, MMMM D YYYY")}</p>
+                        <p className="card-text"><strong>Date End: </strong>{moment(meet.date_end).format("ddd, MMMM D YYYY")}</p>
+                        <p><strong>Venue: </strong>{meet.venue ? meet.venue : 'TBD'}</p>
+                        <p><strong>Location: </strong>{meet.city}, {meet.state} ({meet.country})</p>
+                        <p><strong>Cost: </strong>{meet.cost ? '$' + meet.cost : 'Not listed'}</p>
 
-                          <button onClick="handleSaveMeet" className="btn">Save Meet</button>
-                          <button className="btn"> <a href={meetLink} target="_blank"> Get Directions</a></button>
-                    </div>
-                    <div className="card-block">
-                      <blockquote className="card-blockquote">
-                        <p>{meet.city}, {meet.country}</p>
-
-
-                      </blockquote>
-
-                    </div>
+                        <button onClick={this.handleSaveMeet} className="btn btn-outline-success"><i className="fa fa-calendar-plus-o mr-1" aria-hidden="true"></i>Save Meet</button>
+                        <a className="btn btn-outline-primary ml-3" href={meetLink} target="_blank"><i className="fa fa-location-arrow mr-1" aria-hidden="true"></i>Get Directions</a>
+                  </div>
             </div>
           </div>
         )
